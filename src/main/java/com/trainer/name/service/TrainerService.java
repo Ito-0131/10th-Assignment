2f71a24de81f1bb84c82839fedcb6e280233fe8a
Ito-0131 <berufegoal@yahoo.co.jp>
2024-01-21 15:41:08 +0900

    publicからprivateに変更しました

--- src/main/java/com/trainer/name/service/TrainerService.java
+++ src/main/java/com/trainer/name/service/TrainerService.java
@@ -53,7 +53,7 @@ public class TrainerService {
         return trainerMapper.findById(id).orElseThrow(() -> new TrainerNotFoundException("idが" + id + "のトレーナーはいません"));
     }
 
-    public boolean isEmailUnique(String email) {
+    private boolean isEmailUnique(String email) {
         int count = trainerMapper.countByEmail(email);
         return count == 0;
     }
